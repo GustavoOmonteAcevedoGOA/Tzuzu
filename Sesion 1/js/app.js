2e3ef05cc2cf8b06b4ca9dce6294f72fa757c5f1
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-09-22 22:30:32 -0400

    primera Sesion acabada

---
+++ Sesion 1/js/app.js
@@ -0,0 +1,46 @@
+let edad = 123412;
+let nombre = "Tzuzul";
+let auto = true;
+
+
+const edad1= 3242;
+const nombre1= "Gustavo";
+const auto1= false;
+
+
+
+alert("Hola Amigos");
+
+console.log(`Hola`);
+
+
+function saludar(nombre){
+
+    let saludo = "hola " + nombre;
+    alert(saludo);
+}
+
+saludar("Gustavo");
+
+//imprimir un mensaje con el nombre y la edad de una persona
+
+nombre3= "Gustavo";
+edad3= 25;
+fechaDeNacimiento="15/04/1996";
+function presentar(nombre3,edad3){
+    console.log(`mi nombre es :${nombre} y tengo: ${edad3} años`);
+    
+}
+
+//imprimir un mensaje que nos diga si somos mayores de edad
+
+function mayorOmenor(edad3){
+    (edad3>18)?console.log(`eres mayor de edad`):console.log(`eres menor de edad`);
+        
+}
+
+//imprimir un mensaje con el dia de nuestra fecha de nacimiento
+
+function fecha(){
+    
+}
