a5168207863f4feef6d9389d2197cda41b9a98ad
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-10-25 22:04:44 -0400

    Sesion 11

--- Sesion 10/app.js
+++ Sesion 10/app.js
@@ -31,6 +31,8 @@ console.log(objeto);
 /*--===========se utiliza con librerias o cuando estemos programando en el backend o cuando tengamos un ejercio que requiera varios objetos con varios metodos evitamos copiar el codigo=============--*/
 
 /*--===========clases inician con Mayuscula=============--*/
+//clase padre
+//Super clase
 class Animal{
     constructor(tipoAnimal,nombre,tamaño,sonido){
         this.tipoAnimal=tipoAnimal;
@@ -47,6 +49,50 @@ class Animal{
     };
 }
 
+
+//Herencia
+//clase hijo
+class Ave extends Animal{
+    //juntos super y contructor solo los requeridos de los heredados
+    constructor(volador, tipoAnimal,nombre,tamaño,sonido){
+        super( tipoAnimal,nombre,tamaño,sonido);
+        this.volador = volador;
+        /* super.tipoAnimal = tipoAnimal;
+        super.nombre = nombre;
+        super.tamaño = tamaño;
+        super.sonido = sonido; */
+
+    };
+    volar(distancia){
+        if(this.volador){
+            alert("volando..." + distancia+this.sonido);
+        }else{
+            alert("No estoy volando");
+        }
+
+    };
+}
+
+
+
+//clase hijo
+class Felino extends Animal{
+    //juntos super y contructor solo los requeridos de los heredados
+    constructor(velocidad, tipoAnimal,nombre,sonido){
+        super( tipoAnimal,nombre,"", sonido);
+        this.velocidad = velocidad;
+        /* super.tipoAnimal = tipoAnimal;
+        super.nombre = nombre;
+        super.tamaño = tamaño;
+        super.sonido = sonido; */
+
+    };
+    correr(distancia){
+        alert("corriendo..." + distancia+this.sonido);
+        
+
+    };
+}
 //Objeto nuevo  o instancia de algo
 let gato = new Animal("Felino", "Michi", "pequeño","Mew");
 gato.hacerSonido();
@@ -55,5 +101,31 @@ perro.hacerSonido();
 let gallina = new Animal("Ave", "Gallina", "Muy pequeño","Kikikiki");
 gallina.hacerSonido();
 
+/*--========================--*/
 
+let gallina2 = new Ave(false,"Ave", "Gallina", "Muy pequeño","Kikikiki");
+gallina2.volar(2000);
+let paloma = new Ave(true,"Ave", "Paloma", "pequeño","Kikikiki");
+paloma.volar(2000);
+
+
+/*--========================--*/
+
+let tigre = new Felino(200, "Felino", "tigre", "Grrrr");
+tigre.correr(2000);
+
+//polimorfismo tienen las propiedades en comun y generalizan distintos tipos de objetos
+let animales = [gallina2,paloma,tigre];
+
+for (let i = 0; i < animales.length; i++) {
+    animales[i].hacerSonido();
+    
+}
+
+//trabajar  con clases Cliente  
+//Nuevos
+//Regulares         metodo transferir regresa el 3% de lo que paga
+//Frecuente         metodo transferir regresa el 5% de lo que paga
+
+//metodo transferir dinero de un cliente a otro
 
