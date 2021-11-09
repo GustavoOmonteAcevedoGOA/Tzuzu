8853779d8e792385a3f3622a2c1c0a943adfc772
Gustavo Omonte Acevedo <GustavoGOAweb@gmail.com>
2021-11-08 23:07:21 -0400

    Sesion 14 acabada

--- Sesion 11/app.js
+++ Sesion 11/app.js
@@ -1,7 +1,11 @@
 const select = document.querySelector('#select');
 const opciones = document.querySelector('#opciones');
+const select2 = document.querySelector('#select2');
+const opciones2 = document.querySelector('#opciones2');
 const contenidoSelect = document.querySelector('#select .contenido-select');
+const contenidoSelect2 = document.querySelector('#select2 .contenido-select');
 const hiddenInput = document.querySelector('#inputSelect');
+const hiddenInput2 = document.querySelector('#inputSelect2');
 
 
 document.querySelectorAll('#opciones > .opcion').forEach((opcion) => {
@@ -20,6 +24,23 @@ select.addEventListener('click', () =>{
     select.classList.toggle('active');
     opciones.classList.toggle('active');
 });
-console.log(contenidoSelect);
+
+document.querySelectorAll('#opciones2 > .opcion').forEach((opcion) => {
+    opcion.addEventListener('click', (e) => {
+        e.preventDefault();
+        contenidoSelect2.innerHTML = e.currentTarget.innerHTML;
+        select2.classList.toggle('active');
+        opciones2.classList.toggle('active');
+        hiddenInput2.value = e.currentTarget.querySelector('titulo').innerText;
+    });
+    
+});
+
+
+select2.addEventListener('click', () =>{
+    select2.classList.toggle('active');
+    opciones2.classList.toggle('active');
+});
+
 
 
